05aa6aaab283b78f5b8cc6bb6223bbe8952bf2a2
Ron Waldon <jokeyrhyme@gmail.com>
2015-04-09 17:19:37 +1000

    remove rest of unused pieces of Require.js, start testing

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// 3rd-party modules
+
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+
+// this module
+
+gulp.task('build:lib', [], function () {
+  var main = './loadjs.js';
+  var b = browserify({ entries: main, standalone: 'loadjs' });
+  return b.bundle()
+    .pipe(source(main))
+    .pipe(buffer())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build:tests', [], function () {
+  var main = './tests/index.js';
+  var b = browserify({ entries: main });
+  return b.bundle()
+    .pipe(source(main))
+    .pipe(buffer())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['build:lib', 'build:tests'], function () {});
---
+++ tests/index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// 3rd-party modules
+
+/*eslint-disable vars-on-top*/ // need to fix console first
+
+// jumping through hoops for IE6,7,8
+if (typeof global !== 'undefined') {
+  if (!global.console || !global.console.log) {
+    global.console = require('console');
+  }
+}
+var undo = require('tap-browser-color');
+var test = require('tape');
+
+// our modules
+
+var loadjs = require('../loadjs');
+
+// this module
+
+test('loadjs', function (t) {
+  t.ok(loadjs, 'loadjs module');
+  t.ok(typeof loadjs === 'function', 'exports a function');
+
+  t.test('helloworld.js', function (tt) {
+    loadjs('helloworld.js', function (err, event) {
+      tt.error(err);
+      tt.ok(event, 'callback with event');
+      tt.ok(document.body.innerHTML.indexOf('hello, world!') !== -1);
+      tt.end();
+    });
+  });
+
+  t.test('missing.js', function (tt) {
+    loadjs('missing.js', function (err, event) {
+      if (err) {
+        tt.ok(err instanceof Error, 'callback with error');
+      } else {
+        tt.ok(true);
+      }
+      tt.ok(event, 'callback with event');
+      tt.end();
+    });
+  });
+
+  t.test('badsyntax.js', function (tt) {
+    loadjs('badsyntax.js', function (err, event) {
+      if (err) {
+        tt.ok(err instanceof Error, 'callback with error');
+      } else {
+        tt.ok(true);
+      }
+      tt.ok(event, 'callback with event');
+      tt.end();
+    });
+  });
+
+  t.end();
+});
+
+undo();
