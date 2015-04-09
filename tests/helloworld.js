e258508b552dee95cb81641bddc14744d1d1140e
Ron Waldon <jokeyrhyme@gmail.com>
2015-04-10 07:24:10 +1000

    version missing files (oops)

---
+++ tests/helloworld.js
@@ -0,0 +1,8 @@
+/*eslint-env browser*/
+(function () {
+  'use strict';
+
+  var p = document.createElement('p');
+  p.appendChild(document.createTextNode('hello, world!'));
+  document.body.appendChild(p);
+}());
