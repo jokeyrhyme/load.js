95f26839d982fa0a2358c36824cc41a04cadaee3
Ron Waldon <jokeyrhyme@gmail.com>
2015-06-25 08:57:05 +1000

    use ESLint SemiStandard

--- dist/loadjs.js
+++ dist/loadjs.js
@@ -1,5 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.loadjs = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// https://github.com/jrburke/requirejs/blob/d74e4f1d39/require.js ~ v2.1.17+
+(function (global){
+// https:// github.com/jrburke/requirejs/blob/d74e4f1d39/require.js ~ v2.1.17+
 
 /**
 * @license Available via the MIT or new BSD license.
@@ -10,79 +11,28 @@
 
 'use strict';
 
-var head,
-    baseElement,
-    op = Object.prototype,
-    ostring = op.toString,
-    hasOwn = op.hasOwnProperty,
-    isBrowser = !!(typeof window !== 'undefined' && typeof navigator !== 'undefined' && window.document),
-    isWebWorker = !isBrowser && typeof importScripts !== 'undefined',
-
-//PS3 indicates loaded and complete, but need to wait for complete
-//specifically. Sequence is 'loading', 'loaded', execution,
+var head, baseElement;
+var op = Object.prototype;
+var ostring = op.toString;
+var isBrowser = !!(typeof window !== 'undefined' && global.navigator && global.document);
+var isWebWorker = !isBrowser && typeof importScripts !== 'undefined';
+// PS3 indicates loaded and complete, but need to wait for complete
+// specifically. Sequence is 'loading', 'loaded', execution,
 // then 'complete'. The UA check is unfortunate, but not sure how
-//to feature test w/o causing perf issues.
-readyRegExp = isBrowser && navigator.platform === 'PLAYSTATION 3' ? /^complete$/ : /^(complete|loaded)$/,
-
-//Oh the tragedy, detecting opera. See the usage of isOpera for reason.
-isOpera = typeof opera !== 'undefined' && opera.toString() === '[object Opera]';
+// to feature test w/o causing perf issues.
+var readyRegExp = isBrowser && navigator.platform === 'PLAYSTATION 3' ? /^complete$/ : /^(complete|loaded)$/;
+// Oh the tragedy, detecting opera. See the usage of isOpera for reason.
+var isOpera = typeof opera !== 'undefined' && global.opera.toString() === '[object Opera]';
 
 function isFunction(it) {
   return ostring.call(it) === '[object Function]';
 }
 
-function isArray(it) {
-  return ostring.call(it) === '[object Array]';
-}
-
-function hasProp(obj, prop) {
-  return hasOwn.call(obj, prop);
-}
-
-/**
-* Cycles over properties in an object and calls a function for each
-* property value. If the function returns a truthy value, then the
-* iteration is stopped.
-*/
-function eachProp(obj, func) {
-  var prop;
-  for (prop in obj) {
-    if (hasProp(obj, prop)) {
-      if (func(obj[prop], prop)) {
-        break;
-      }
-    }
-  }
-}
-
-/**
-* Simple function to mix in properties from source into target,
-* but only if target does not already have a property of the same name.
-*/
-function mixin(target, source, force, deepStringMixin) {
-  if (source) {
-    eachProp(source, function (value, prop) {
-      if (force || !hasProp(target, prop)) {
-        if (deepStringMixin && typeof value === 'object' && value && !isArray(value) && !isFunction(value) && !(value instanceof RegExp)) {
-
-          if (!target[prop]) {
-            target[prop] = {};
-          }
-          mixin(target[prop], value, force, deepStringMixin);
-        } else {
-          target[prop] = value;
-        }
-      }
-    });
-  }
-  return target;
-}
-
 if (isBrowser) {
   head = document.getElementsByTagName('head')[0];
-  //If BASE tag is in play, using appendChild is a problem for IE6.
-  //When that browser dies, this can be removed. Details in this jQuery bug:
-  //http://dev.jquery.com/ticket/2709
+  // If BASE tag is in play, using appendChild is a problem for IE6.
+  // When that browser dies, this can be removed. Details in this jQuery bug:
+  // http:// dev.jquery.com/ticket/2709
   baseElement = document.getElementsByTagName('base')[0];
   if (baseElement) {
     head = baseElement.parentNode;
@@ -95,7 +45,7 @@ if (isBrowser) {
 function createNode() {
   // possibly support non-default namespace one day
   // var node = config.xhtml ?
-  // document.createElementNS('http://www.w3.org/1999/xhtml', 'html:script') :
+  // document.createElementNS('http:// www.w3.org/1999/xhtml', 'html:script') :
   var node = document.createElement('script');
   node.type = 'text/javascript';
   node.charset = 'utf-8';
@@ -104,12 +54,12 @@ function createNode() {
 }
 
 function removeListener(node, func, name, ieName) {
-  //Favor detachEvent because of IE9
-  //issue, see attachEvent/addEventListener comment elsewhere
-  //in this file.
+  // Favor detachEvent because of IE9
+  // issue, see attachEvent/addEventListener comment elsewhere
+  // in this file.
   if (node.detachEvent && !isOpera) {
-    //Probably IE. If not it will throw an error, which will be
-    //useful to know.
+    // Probably IE. If not it will throw an error, which will be
+    // useful to know.
     if (ieName) {
       node.detachEvent(ieName, func);
     }
@@ -142,9 +92,9 @@ function loadjs(url, callback) {
   };
 
   onScriptLoad = function (evt) {
-    //Using currentTarget instead of target for Firefox 2.0's sake. Not
-    //all old browsers will be supported, but this one was easy enough
-    //to support and still makes sense.
+    // Using currentTarget instead of target for Firefox 2.0's sake. Not
+    // all old browsers will be supported, but this one was easy enough
+    // to support and still makes sense.
     if (evt.type === 'load' || readyRegExp.test((evt.currentTarget || evt.srcElement).readyState)) {
       completeLoad(null, evt);
     }
@@ -155,38 +105,38 @@ function loadjs(url, callback) {
   };
 
   if (isBrowser) {
-    //In the browser so use a script tag
+    // In the browser so use a script tag
     node = createNode();
 
-    //Set up load listener. Test attachEvent first because IE9 has
-    //a subtle issue in its addEventListener and script onload firings
-    //that do not match the behavior of all other browsers with
-    //addEventListener support, which fire the onload event for a
-    //script right after the script execution. See:
-    //https://connect.microsoft.com/IE/feedback/details/648057/script-onload-event-is-not-fired-immediately-after-script-execution
-    //UNFORTUNATELY Opera implements attachEvent but does not follow the script
-    //script execution mode.
+    // Set up load listener. Test attachEvent first because IE9 has
+    // a subtle issue in its addEventListener and script onload firings
+    // that do not match the behavior of all other browsers with
+    // addEventListener support, which fire the onload event for a
+    // script right after the script execution. See:
+    // https:// connect.microsoft.com/IE/feedback/details/648057/script-onload-event-is-not-fired-immediately-after-script-execution
+    // UNFORTUNATELY Opera implements attachEvent but does not follow the script
+    // script execution mode.
     if (node.attachEvent &&
-    //Check if node.attachEvent is artificially added by custom script or
-    //natively supported by browser
-    //read https://github.com/jrburke/requirejs/issues/187
-    //if we can NOT find [native code] then it must NOT natively supported.
-    //in IE8, node.attachEvent does not have toString()
-    //Note the test for "[native code" with no closing brace, see:
-    //https://github.com/jrburke/requirejs/issues/273
+    // Check if node.attachEvent is artificially added by custom script or
+    // natively supported by browser
+    // read https:// github.com/jrburke/requirejs/issues/187
+    // if we can NOT find [native code] then it must NOT natively supported.
+    // in IE8, node.attachEvent does not have toString()
+    // Note the test for "[native code" with no closing brace, see:
+    // https:// github.com/jrburke/requirejs/issues/273
     !(node.attachEvent.toString && node.attachEvent.toString().indexOf('[native code') < 0) && !isOpera) {
       node.attachEvent('onreadystatechange', onScriptLoad);
-      //It would be great to add an error handler here to catch
-      //404s in IE9+. However, onreadystatechange will fire before
-      //the error handler, so that does not help. If addEventListener
-      //is used, then IE will fire error before load, but we cannot
-      //use that pathway given the connect.microsoft.com issue
-      //mentioned above about not doing the 'script execute,
-      //then fire the script load event listener before execute
-      //next script' that other browsers do.
-      //Best hope: IE10 fixes the issues,
-      //and then destroys all installs of IE 6-9.
-      //node.attachEvent('onerror', onScriptError);
+      // It would be great to add an error handler here to catch
+      // 404s in IE9+. However, onreadystatechange will fire before
+      // the error handler, so that does not help. If addEventListener
+      // is used, then IE will fire error before load, but we cannot
+      // use that pathway given the connect.microsoft.com issue
+      // mentioned above about not doing the 'script execute,
+      // then fire the script load event listener before execute
+      // next script' that other browsers do.
+      // Best hope: IE10 fixes the issues,
+      // and then destroys all installs of IE 6-9.
+      // node.attachEvent('onerror', onScriptError);
     } else {
       node.addEventListener('load', onScriptLoad, false);
       node.addEventListener('error', onScriptError, false);
@@ -203,15 +153,15 @@ function loadjs(url, callback) {
   }
   if (isWebWorker) {
     try {
-      //In a web worker, use importScripts. This is not a very
-      //efficient use of importScripts, importScripts will block until
-      //its script is downloaded and evaluated. However, if web workers
-      //are in play, the expectation that a build has been done so that
-      //only one script needs to be loaded anyway. This may need to be
-      //reevaluated if other use cases become common.
+      // In a web worker, use importScripts. This is not a very
+      // efficient use of importScripts, importScripts will block until
+      // its script is downloaded and evaluated. However, if web workers
+      // are in play, the expectation that a build has been done so that
+      // only one script needs to be loaded anyway. This may need to be
+      // reevaluated if other use cases become common.
       importScripts(url);
 
-      //Account for anonymous modules
+      // Account for anonymous modules
       completeLoad(null);
     } catch (e) {
       completeLoad(new Error('loadjs error: ' + url), null);
@@ -221,5 +171,6 @@ function loadjs(url, callback) {
 
 module.exports = loadjs;
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}]},{},[1])(1)
 });
--- loadjs.js
+++ loadjs.js
@@ -1,4 +1,4 @@
-// https://github.com/jrburke/requirejs/blob/d74e4f1d39/require.js ~ v2.1.17+
+// https:// github.com/jrburke/requirejs/blob/d74e4f1d39/require.js ~ v2.1.17+
 
 /**
 * @license Available via the MIT or new BSD license.
@@ -9,79 +9,29 @@
 
 'use strict';
 
-var head, baseElement,
-op = Object.prototype,
-ostring = op.toString,
-hasOwn = op.hasOwnProperty,
-isBrowser = !!(typeof window !== 'undefined' && typeof navigator !== 'undefined' && window.document),
-isWebWorker = !isBrowser && typeof importScripts !== 'undefined',
-//PS3 indicates loaded and complete, but need to wait for complete
-//specifically. Sequence is 'loading', 'loaded', execution,
+var head, baseElement;
+var op = Object.prototype;
+var ostring = op.toString;
+var isBrowser = !!(typeof window !== 'undefined' && global.navigator && global.document);
+var isWebWorker = !isBrowser && typeof importScripts !== 'undefined';
+// PS3 indicates loaded and complete, but need to wait for complete
+// specifically. Sequence is 'loading', 'loaded', execution,
 // then 'complete'. The UA check is unfortunate, but not sure how
-//to feature test w/o causing perf issues.
-readyRegExp = isBrowser && navigator.platform === 'PLAYSTATION 3' ?
-/^complete$/ : /^(complete|loaded)$/,
-//Oh the tragedy, detecting opera. See the usage of isOpera for reason.
-isOpera = typeof opera !== 'undefined' && opera.toString() === '[object Opera]';
+// to feature test w/o causing perf issues.
+var readyRegExp = isBrowser && navigator.platform === 'PLAYSTATION 3' ?
+/^complete$/ : /^(complete|loaded)$/;
+// Oh the tragedy, detecting opera. See the usage of isOpera for reason.
+var isOpera = typeof opera !== 'undefined' && global.opera.toString() === '[object Opera]';
 
-function isFunction(it) {
+function isFunction (it) {
   return ostring.call(it) === '[object Function]';
-}
-
-function isArray(it) {
-  return ostring.call(it) === '[object Array]';
-}
-
-function hasProp(obj, prop) {
-  return hasOwn.call(obj, prop);
-}
-
-/**
-* Cycles over properties in an object and calls a function for each
-* property value. If the function returns a truthy value, then the
-* iteration is stopped.
-*/
-function eachProp(obj, func) {
-  var prop;
-  for (prop in obj) {
-    if (hasProp(obj, prop)) {
-      if (func(obj[prop], prop)) {
-        break;
-      }
-    }
-  }
-}
-
-/**
-* Simple function to mix in properties from source into target,
-* but only if target does not already have a property of the same name.
-*/
-function mixin(target, source, force, deepStringMixin) {
-  if (source) {
-    eachProp(source, function (value, prop) {
-      if (force || !hasProp(target, prop)) {
-        if (deepStringMixin && typeof value === 'object' && value &&
-        !isArray(value) && !isFunction(value) &&
-        !(value instanceof RegExp)) {
-
-          if (!target[prop]) {
-            target[prop] = {};
-          }
-          mixin(target[prop], value, force, deepStringMixin);
-        } else {
-          target[prop] = value;
-        }
-      }
-    });
-  }
-  return target;
 }
 
 if (isBrowser) {
   head = document.getElementsByTagName('head')[0];
-  //If BASE tag is in play, using appendChild is a problem for IE6.
-  //When that browser dies, this can be removed. Details in this jQuery bug:
-  //http://dev.jquery.com/ticket/2709
+  // If BASE tag is in play, using appendChild is a problem for IE6.
+  // When that browser dies, this can be removed. Details in this jQuery bug:
+  // http:// dev.jquery.com/ticket/2709
   baseElement = document.getElementsByTagName('base')[0];
   if (baseElement) {
     head = baseElement.parentNode;
@@ -91,10 +41,10 @@ if (isBrowser) {
 /**
 * Creates the node for the load command. Only used in browser envs.
 */
-function createNode() {
+function createNode () {
   // possibly support non-default namespace one day
   // var node = config.xhtml ?
-  // document.createElementNS('http://www.w3.org/1999/xhtml', 'html:script') :
+  // document.createElementNS('http:// www.w3.org/1999/xhtml', 'html:script') :
   var node = document.createElement('script');
   node.type = 'text/javascript';
   node.charset = 'utf-8';
@@ -102,13 +52,13 @@ function createNode() {
   return node;
 }
 
-function removeListener(node, func, name, ieName) {
-  //Favor detachEvent because of IE9
-  //issue, see attachEvent/addEventListener comment elsewhere
-  //in this file.
+function removeListener (node, func, name, ieName) {
+  // Favor detachEvent because of IE9
+  // issue, see attachEvent/addEventListener comment elsewhere
+  // in this file.
   if (node.detachEvent && !isOpera) {
-    //Probably IE. If not it will throw an error, which will be
-    //useful to know.
+    // Probably IE. If not it will throw an error, which will be
+    // useful to know.
     if (ieName) {
       node.detachEvent(ieName, func);
     }
@@ -141,9 +91,9 @@ function loadjs (url, callback) {
   };
 
   onScriptLoad = function (evt) {
-    //Using currentTarget instead of target for Firefox 2.0's sake. Not
-    //all old browsers will be supported, but this one was easy enough
-    //to support and still makes sense.
+    // Using currentTarget instead of target for Firefox 2.0's sake. Not
+    // all old browsers will be supported, but this one was easy enough
+    // to support and still makes sense.
     if (
       evt.type === 'load' ||
       readyRegExp.test((evt.currentTarget || evt.srcElement).readyState)
@@ -157,41 +107,41 @@ function loadjs (url, callback) {
   };
 
   if (isBrowser) {
-    //In the browser so use a script tag
+    // In the browser so use a script tag
     node = createNode();
 
-    //Set up load listener. Test attachEvent first because IE9 has
-    //a subtle issue in its addEventListener and script onload firings
-    //that do not match the behavior of all other browsers with
-    //addEventListener support, which fire the onload event for a
-    //script right after the script execution. See:
-    //https://connect.microsoft.com/IE/feedback/details/648057/script-onload-event-is-not-fired-immediately-after-script-execution
-    //UNFORTUNATELY Opera implements attachEvent but does not follow the script
-    //script execution mode.
+    // Set up load listener. Test attachEvent first because IE9 has
+    // a subtle issue in its addEventListener and script onload firings
+    // that do not match the behavior of all other browsers with
+    // addEventListener support, which fire the onload event for a
+    // script right after the script execution. See:
+    // https:// connect.microsoft.com/IE/feedback/details/648057/script-onload-event-is-not-fired-immediately-after-script-execution
+    // UNFORTUNATELY Opera implements attachEvent but does not follow the script
+    // script execution mode.
     if (
       node.attachEvent &&
-      //Check if node.attachEvent is artificially added by custom script or
-      //natively supported by browser
-      //read https://github.com/jrburke/requirejs/issues/187
-      //if we can NOT find [native code] then it must NOT natively supported.
-      //in IE8, node.attachEvent does not have toString()
-      //Note the test for "[native code" with no closing brace, see:
-      //https://github.com/jrburke/requirejs/issues/273
+      // Check if node.attachEvent is artificially added by custom script or
+      // natively supported by browser
+      // read https:// github.com/jrburke/requirejs/issues/187
+      // if we can NOT find [native code] then it must NOT natively supported.
+      // in IE8, node.attachEvent does not have toString()
+      // Note the test for "[native code" with no closing brace, see:
+      // https:// github.com/jrburke/requirejs/issues/273
       !(node.attachEvent.toString && node.attachEvent.toString().indexOf('[native code') < 0) &&
       !isOpera
     ) {
       node.attachEvent('onreadystatechange', onScriptLoad);
-      //It would be great to add an error handler here to catch
-      //404s in IE9+. However, onreadystatechange will fire before
-      //the error handler, so that does not help. If addEventListener
-      //is used, then IE will fire error before load, but we cannot
-      //use that pathway given the connect.microsoft.com issue
-      //mentioned above about not doing the 'script execute,
-      //then fire the script load event listener before execute
-      //next script' that other browsers do.
-      //Best hope: IE10 fixes the issues,
-      //and then destroys all installs of IE 6-9.
-      //node.attachEvent('onerror', onScriptError);
+      // It would be great to add an error handler here to catch
+      // 404s in IE9+. However, onreadystatechange will fire before
+      // the error handler, so that does not help. If addEventListener
+      // is used, then IE will fire error before load, but we cannot
+      // use that pathway given the connect.microsoft.com issue
+      // mentioned above about not doing the 'script execute,
+      // then fire the script load event listener before execute
+      // next script' that other browsers do.
+      // Best hope: IE10 fixes the issues,
+      // and then destroys all installs of IE 6-9.
+      // node.attachEvent('onerror', onScriptError);
     } else {
       node.addEventListener('load', onScriptLoad, false);
       node.addEventListener('error', onScriptError, false);
@@ -208,15 +158,15 @@ function loadjs (url, callback) {
   }
   if (isWebWorker) {
     try {
-      //In a web worker, use importScripts. This is not a very
-      //efficient use of importScripts, importScripts will block until
-      //its script is downloaded and evaluated. However, if web workers
-      //are in play, the expectation that a build has been done so that
-      //only one script needs to be loaded anyway. This may need to be
-      //reevaluated if other use cases become common.
+      // In a web worker, use importScripts. This is not a very
+      // efficient use of importScripts, importScripts will block until
+      // its script is downloaded and evaluated. However, if web workers
+      // are in play, the expectation that a build has been done so that
+      // only one script needs to be loaded anyway. This may need to be
+      // reevaluated if other use cases become common.
       importScripts(url);
 
-      //Account for anonymous modules
+      // Account for anonymous modules
       completeLoad(null);
     } catch (e) {
       completeLoad(new Error('loadjs error: ' + url), null);
